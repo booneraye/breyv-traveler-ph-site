63797e5a77e86833361e04729fb689ff251a4632
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-14 06:46:20 +0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -11,7 +11,7 @@ module.exports = {
   authorUrl: 'https://breyvtravelerph.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@booneraye', // Change for Twitter Cards
   shortTitle: 'BREYV TRAVELER PH', // Used for App manifest e.g. Mobile Home Screen
-  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImage: '/logos/logo1.png', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo1.png', // Logo used for SEO, RSS, and App manifest
