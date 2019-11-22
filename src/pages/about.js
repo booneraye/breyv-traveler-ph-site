c0c76a5a4d60cd69ea8bd3e58438bd456cceac55
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-22 14:47:09 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
+import aboutImage './about-breyv.jpg'
 
 const About = ({ data }) => {
   const postNode = {
@@ -18,7 +19,7 @@ const About = ({ data }) => {
         <title>{`About - ${config.siteTitle}`}</title>
       </Helmet>
       <SEO postNode={postNode} pagePath="contact" customTitle />
-      <Hero title={'About Cover'} image={'./about-breyv.jpg'} height={'50vh'} />
+      <Hero title={'About Cover'} image={aboutImage} height={'50vh'} />
       <Container>
         <PageTitle>About</PageTitle>
         <div>Many people are hindered to travel and explore their own country not because the lack of funds but because the lack of confidence to go outside their own boxes and eagerness to see the world - the bigger world that might have.</div>
