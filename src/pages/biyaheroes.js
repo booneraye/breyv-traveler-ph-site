0fd40dfd498d300b7f9656e1bbdd145e0d3dd3e2
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-24 23:35:27 +0800

    Create biyaheroes.js

---
+++ src/pages/biyaheroes.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import config from '../utils/siteConfig'
+import Layout from '../components/Layout'
+import Container from '../components/Container'
+import PageTitle from '../components/PageTitle'
+import ContactForm from '../components/ContactForm'
+import SEO from '../components/SEO'
+
+const Biyaheroes = ({ data }) => {
+  const postNode = {
+    title: `Biyaheroes - ${config.siteTitle}`,
+  }
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{`Biyaheroes - ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postNode={postNode} pagePath="biyaheroes" customTitle />
+
+      <Container>
+        <PageTitle>Biyaheroes</PageTitle>
+        <div id="biyaheroes-widget-container">
+	        <div id="biyaheroes-widget-brave-traveler-ph-566db44c6bb873ae1cd8662e67708577e916940fa1e37aff4965b1a33299f688eda1b27133d1c338ddfe2c9c20bacd2f">
+	        </div>
+	        <script type="application/javascript" src="https://dev-biz.biyaheroes.com/widget/brave-traveler-ph/566db44c6bb873ae1cd8662e67708577e916940fa1e37aff4965b1a33299f688eda1b27133d1c338ddfe2c9c20bacd2f"></script>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Biyaheroes
