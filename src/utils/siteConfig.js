96f09ed24a96f42daf14a0903ec0994aa54c21ca
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-24 23:15:24 +0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,21 @@
 module.exports = {
-  siteTitle: 'BREYV TRAVELER PH',
-  siteTitleAlt: 'BREYV TRAVELER PH', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named BREYV TRAVELER PH', // Organization name used for SEO schema
+  siteTitle: 'BRAVE TRAVELER PH',
+  siteTitleAlt: 'BRAVE TRAVELER PH', // This allows an alternative site title for SEO schema.
+  publisher: 'Publisher named BRAVE TRAVELER PH', // Organization name used for SEO schema
   siteDescription:
-    'BREYV TRAVELER PH AIMS TO HELP PEOPLE TO TRAVEL',
+    'Brave traveler ph started as a dream that travelling, especially in the Philippines will soon become easier and adventurous by giving the people tips, encouragement, ideas and support on how to explore the beauty of our own country. It is an idealism that you dont have need a lot of funds or money to be able to travel, you just need to be BRAVE enough to do so.',
   siteUrl: 'https://breyvtraveler.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
   author: 'Boone Raye Flores', // Author for RSS author segment and SEO schema
   authorUrl: 'https://breyvtraveler.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@booneraye', // Change for Twitter Cards
-  shortTitle: 'BREYV TRAVELER PH', // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: 'BRAVE TRAVELER PH', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/46524267_2102808913091549_3585510914280914944_o.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo1.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 BREYV TRAVELER PH', // Copyright string for the RSS feed
+  copyright: 'Copyright © 2018 BRAVE TRAVELER PH', // Copyright string for the RSS feed
 }
