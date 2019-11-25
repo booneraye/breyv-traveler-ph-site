445deeae11c1d1715e9d6c10093b3cea9c71134a
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-25 16:11:23 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -12,11 +12,11 @@ const Biyaheroes = ({ data }) => {
     title: `Biyaheroes - ${config.siteTitle}`,
   }
   
-  const booking = `<div id="biyaheroes-widget-container">
+  const booking = `<div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 500px"><div id="biyaheroes-widget-container" style>
 		<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
 		</div>
 		<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705"></script>
-	</div>`;
+	</div></div>`;
 
   return (
 	  <Layout>
