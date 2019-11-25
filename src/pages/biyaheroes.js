e443a0aa41325f639aedd3b436a00550c4eb29bd
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-25 14:58:29 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -13,25 +13,25 @@ const Biyaheroes = ({ data }) => {
   }
 
   return (
-    <Layout>
-      <Helmet>
-        <title>{`Biyaheroes - ${config.siteTitle}`}</title>
-      </Helmet>
-      <SEO postNode={postNode} pagePath="biyaheroes" customTitle />
+	  <React.Fragment>
+	  	<Layout>
+      			<Helmet>
+      				<title>{`Biyaheroes - ${config.siteTitle}`}</title>
+      			</Helmet>
+      			<SEO postNode={postNode} pagePath="biyaheroes" customTitle />
 	
-      <Container>
-        <PageTitle>Biyaheroes</PageTitle>
-	
-	
-        <div id="biyaheroes-widget-container">
-		<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
-		</div>
-	</div>
-	<Helmet>
-		<script src="https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705" />
-	</Helmet>
-      </Container>	
-    </Layout>
+      			<Container>
+        			<PageTitle>Biyaheroes</PageTitle>
+        			<div id="biyaheroes-widget-container">
+					<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
+					</div>
+				</div>
+				<Helmet>
+					<script src="https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705" />
+				</Helmet>
+      			</Container>	
+    		</Layout>
+	</React.Fragment>
   )
 }
 
