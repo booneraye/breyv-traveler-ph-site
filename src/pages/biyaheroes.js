204b57823128b7902f748666bf27e4991fa2d1e7
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-25 16:17:58 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -27,9 +27,10 @@ const Biyaheroes = ({ data }) => {
 				<div id="biyaheroes-widget-container">
 					<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
 					</div>
+					<div dangerouslySetInnerHTML={{ __html: booking }}/>
 				</div>
 			</div>
-        		<div dangerouslySetInnerHTML={{ __html: booking }}/>
+        		
       		</Container>
     	</Layout>
   )
