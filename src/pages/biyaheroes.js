afed456bf14ee8ee470b5fedc5aeb0267455f32d
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-28 20:08:27 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -12,13 +12,13 @@ const Biyaheroes = ({ data }) => {
     title: `Biyaheroes - ${config.siteTitle}`,
   }
   
+  const scriptSrc = `<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705"></script>`;
+	
   const booking = `<div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 100%; height: 600px">
 			<div id="biyaheroes-widget-container">
 				<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
 				</div>	
-			</div>
-		</div>`;
-	const scriptSrc = `<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705"></script>`;
+			</div> ${scriptSrc} </div>`;
 
   return (
 	  <Layout>
@@ -29,7 +29,7 @@ const Biyaheroes = ({ data }) => {
 	
       		<Container>
         		<PageTitle>Biyaheroes</PageTitle>
-        		<div dangerouslySetInnerHTML={{ __html: booking + scriptSrc }}/>
+        		<div dangerouslySetInnerHTML={{ __html: booking }}/>
       		</Container>
     	</Layout>
   )
