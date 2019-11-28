08cccf8682d86728e1ef51b09802c657626a068e
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-11-28 20:39:07 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -8,6 +8,10 @@ import ContactForm from '../components/ContactForm'
 import SEO from '../components/SEO'
 import useScript from 'hooks/useScript';
 
+const BookingService = {
+  	useScript('https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705');
+  }
+
 const Biyaheroes = ({ data }) => {
   const postNode = {
     title: `Biyaheroes - ${config.siteTitle}`,
@@ -18,9 +22,6 @@ const Biyaheroes = ({ data }) => {
 				<div id="biyaheroes-widget-brave-traveler-ph-be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705">
 				</div>	
 			</div> </div>`;
-  const BookingService = {
-  	useScript('https://biz.biyaheroes.com/widget/brave-traveler-ph/be70440e47bf3c0fda285ed94e9508e17b4857f53608d36fd64f88ac32bb376d78128207cc4b443b9b6d763f2afd8705');
-  }
 
   return (
 	  <Layout>
