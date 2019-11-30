1355c5358c9e9ade0cc6322d15d536cca686d38c
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 03:13:57 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -7,6 +7,7 @@ import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import ContactForm from '../components/ContactForm'
 import SEO from '../components/SEO'
+import BookingForm from '../components/BookingForm'
 
 
 const Biyaheroes = ({ data }) => {
@@ -14,13 +15,6 @@ const Biyaheroes = ({ data }) => {
     title: `Biyaheroes - ${config.siteTitle}`,
   }
 	
-  const booking = `<div style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 100%; height: 600px">
-			<div id="biyaheroes-widget-container">
-	<div id="biyaheroes-widget-brave-traveler-ph-website-0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77">
-	</div>
-	<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph-website/0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77"></script>
-</div></div>`;
-
   return (
 	  <Layout>
       		<Helmet>
@@ -30,7 +24,7 @@ const Biyaheroes = ({ data }) => {
 	
       		<Container>
         		<PageTitle>Biyaheroes</PageTitle>
-        		<div dangerouslySetInnerHTML={{ __html: booking }}/>
+        		<BookingForm/>
       		</Container>
     	</Layout>
   )
