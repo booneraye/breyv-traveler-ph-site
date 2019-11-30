ed41916da9aa2eebdaef8e8b5725528a397ae36e
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 02:40:19 +0800

    Create BookingForm.js

---
+++ src/components/BookingForm.js
@@ -0,0 +1,13 @@
+const React = require("react")
+exports.onRenderBody = ({
+  setPostBodyComponents,
+}) => {
+  
+  setPostBodyComponents([
+    <script 
+      type="application/javascript"
+      src="https://biz.biyaheroes.com/widget/brave-traveler-ph-website/0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77">
+    </script>
+,
+  ])
+}
