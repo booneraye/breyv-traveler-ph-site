9f9e9061dbc25bc1bcc72f65b6b5c6ae878a47ec
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 02:42:13 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -7,7 +7,7 @@ import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import ContactForm from '../components/ContactForm'
 import SEO from '../components/SEO'
-
+import BookingForm from '../components/BookingForm'
 
 
 const Biyaheroes = ({ data }) => {
@@ -33,9 +33,7 @@ const Biyaheroes = ({ data }) => {
         		<PageTitle>Biyaheroes</PageTitle>
         		<div dangerouslySetInnerHTML={{ __html: booking }}/>
       		</Container>
-		<Helmet>
-			<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph-website/0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77"></script>
-		</Helmet>
+		<BookingForm /> 
     	</Layout>
   )
 }
