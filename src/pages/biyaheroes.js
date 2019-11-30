34a6e123eab565c82db4e4c348bc1efddc3633e2
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 02:18:50 +0800

    Update biyaheroes.js

--- src/pages/biyaheroes.js
+++ src/pages/biyaheroes.js
@@ -19,7 +19,7 @@ const Biyaheroes = ({ data }) => {
 			<div id="biyaheroes-widget-container">
 	<div id="biyaheroes-widget-brave-traveler-ph-website-0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77">
 	</div>
-	<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph-website/0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77"></script>
+	
 </div></div>`;
 
   return (
@@ -33,6 +33,9 @@ const Biyaheroes = ({ data }) => {
         		<PageTitle>Biyaheroes</PageTitle>
         		<div dangerouslySetInnerHTML={{ __html: booking }}/>
       		</Container>
+		<Helmet>
+			<script type="application/javascript" src="https://biz.biyaheroes.com/widget/brave-traveler-ph-website/0c246ad427714b5190e9b9651b1ec51cbbe656078c47a39e5159a788ecbf3dced973716cba12a16f95900c6694e87c77"></script>
+		</Helmet>
     	</Layout>
   )
 }
