8cbbd62e8a0e8714fcad9d3fa057bbd51376be4f
booneraye <booneraye@gmail.com>
2019-12-02 03:03:41 +0800

    update

--- src/components/GoogleAdsFeeds.js
+++ src/components/GoogleAdsFeeds.js
@@ -5,7 +5,7 @@ const GoogleAdsFeeds = () => (
     <AdSense.Google
         client='ca-pub-2901559959048252'
         slot='8492202498'
-        style={{ display: 'block', height: '30vh', width: '500px', margin: '0 auto'}}
+        style={{ display: 'block', height: '30vh', width: '80%', margin: '0 auto'}}
         format='fluid'
         responsive='true'
         layoutKey='-6h+dd+4d-5u+3c'
