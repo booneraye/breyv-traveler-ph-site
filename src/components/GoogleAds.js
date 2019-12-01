00ceb5e2b850da1a2eb5a9c5c31fd7da41d5b0f2
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 23:24:12 +0800

    Update GoogleAds.js

--- src/components/GoogleAds.js
+++ src/components/GoogleAds.js
@@ -2,21 +2,19 @@ import React from 'react'
 
 const GoogleAds = () => {
   
-  function googleAds(){ 
-    return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- first ads -->
-         <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-2901559959048252"
-          data-ad-slot="6826970879"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>` };
+  const googleAds = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-6h+dd+4d-5u+3c"
+     data-ad-client="ca-pub-2901559959048252"
+     data-ad-slot="8492202498"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>` ;
   
   return (
-        <div dangerouslySetInnerHTML={{__html: googleAds()}} />
+        <div dangerouslySetInnerHTML={{__html: googleAds }} />
   )
 }
 
