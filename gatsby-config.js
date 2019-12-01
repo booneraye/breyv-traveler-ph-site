f859a84f9dee4feab9d3e12f06a251826f3930d7
booneraye <booneraye@gmail.com>
2019-12-02 02:35:41 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -170,5 +170,12 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-routes`,
+      options: {
+        // this is the path to your routes configuration file
+        path: `${__dirname}/src/utils/routes.js`,
+      },
+    },
   ],
 }
---
+++ src/utils/routes.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = [
+  {
+    path: '/ads.txt',
+    component: path.resolve(`ads.txt`)
+  },
+];
