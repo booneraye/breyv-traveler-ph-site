aca267e599394958b63ead1ddade4983b9de5b39
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 23:25:57 +0800

    Update index.js

--- src/templates/index.js
+++ src/templates/index.js
@@ -8,6 +8,7 @@ import Container from '../components/Container'
 import Pagination from '../components/Pagination'
 import SEO from '../components/SEO'
 import config from '../utils/siteConfig'
+import GoogleAds from'../components/GoogleAds'
 
 const Index = ({ data, pageContext }) => {
   const posts = data.allContentfulPost.edges
@@ -38,6 +39,7 @@ const Index = ({ data, pageContext }) => {
             ))}
           </CardList>
         )}
+        <GoogleAds/>
       </Container>
       <Pagination context={pageContext} />
     </Layout>
