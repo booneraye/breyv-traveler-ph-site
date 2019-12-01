8dd83cac460656d201d9e651f041cfedf6a7a8ba
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 22:48:52 +0800

    Update GoogleAds.js

--- src/components/GoogleAds.js
+++ src/components/GoogleAds.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import Helmet from'../components/Helmet'
 
 const GoogleAds = () => {
   
-  function googleAds(){ return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- first ads -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2901559959048252"
-     data-ad-slot="6826970879"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>` };
+  function googleAds(){ 
+    return `
+       <!-- first ads -->
+         <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-2901559959048252"
+          data-ad-slot="6826970879"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>` };
   
   return (
+    <Helmet>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    </Helmet>
     <div dangerouslySetInnerHTML={{__html: googleAds()}} />
   )
 }
