eb403bca9f21bd183af13fde5afee3f56455615a
booneraye <booneraye@gmail.com>
2019-12-02 01:50:13 +0800

    update google ads

--- src/components/GoogleAdsArticle.js
+++ src/components/GoogleAdsArticle.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import AdSense from 'react-adsense';
 
-const GoogleAdsFeeds = () => (
+const GoogleAdsArticle = () => (
     <AdSense.Google
         client='ca-pub-2901559959048252'
         slot='8492202498'
-        style={{ display: 'block', height: '30vh', width: '60%', margin: '0 auto'}}
+        style={{ display: 'block', height: '30vh'}}
         format='fluid'
         responsive='true'
         layoutKey='-6h+dd+4d-5u+3c'
     />
 );
 
-export default GoogleAdsFeeds;
+export default GoogleAdsArticle;
