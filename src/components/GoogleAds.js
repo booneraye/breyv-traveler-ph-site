683da5a7e838869255ba8ff9e3292727d5fdc9d6
booneraye <booneraye@gmail.com>
2019-12-02 00:21:10 +0800

    update

--- src/components/GoogleAds.js
+++ src/components/GoogleAds.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import AdSense from 'react-adsense';
 
-const GoogleAds = () => (
+const GoogleAdsDisplay = () => (
     <AdSense.Google
         client='ca-pub-2901559959048252'
         slot='6826970879'
-        style={{ display: 'block' }}
+        style={{ display: 'block', height: '100px' }}
         format='auto'
         responsive='true'
     />
 );
 
-export default GoogleAds;
+export default GoogleAdsDisplay;
