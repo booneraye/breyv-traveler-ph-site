cc3673b39b6a1951fa942bfaa34932f281a17ee5
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2019-12-01 22:38:50 +0800

    Update page.js

--- src/templates/page.js
+++ src/templates/page.js
@@ -7,7 +7,7 @@ import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import PageBody from '../components/PageBody'
 import SEO from '../components/SEO'
-import GoogleAds '../components/GoogleAds'
+import GoogleAds from '../components/GoogleAds'
 
 const PageTemplate = ({ data }) => {
   const { title, slug, body } = data.contentfulPage
