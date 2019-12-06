a1c2d03610f2a71b6ea7e39fc65d5fb83a945903
booneraye <booneraye@gmail.com>
2019-12-06 14:07:55 +0800

    UPDATE GOOGLE ADS

--- src/components/GoogleAdsDisplay.js
+++ src/components/GoogleAdsDisplay.js
@@ -5,7 +5,7 @@ const GoogleAdsDisplay = () => (
     <AdSense.Google
         client='ca-pub-2901559959048252'
         slot='6826970879'
-        style={{ display: 'block', height: '50%', width: '80%', margin: '0 auto', paddingBottom: '2%'}}
+        style={{ display: 'block'}}
         format='auto'
         responsive='true'
     />
