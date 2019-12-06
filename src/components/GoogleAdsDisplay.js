e3738f02320cc0e9c778bfb0a1b5f264c136458e
booneraye <booneraye@gmail.com>
2019-12-06 14:35:41 +0800

    update google ads

--- src/components/GoogleAdsDisplay.js
+++ src/components/GoogleAdsDisplay.js
@@ -3,12 +3,8 @@ import AdSense from 'react-adsense';
 
 const ads = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`;
 
-const Adservice = () => (
-    <div dangerouslySetInnerHTML={ ads } />
-);
-
 const GoogleAdsDisplay = () => (
-    <Adservice />
+    <div dangerouslySetInnerHTML={ ads } />
     <AdSense.Google
         client='ca-pub-2901559959048252'
         slot='6826970879'
