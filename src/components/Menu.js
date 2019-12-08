419c293b99585057b5b021f8fa90371ebe30b1dc
booneraye <booneraye@gmail.com>
2019-12-09 04:53:49 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import header from '../styles/header.css'
-import Helmet from 'react-helmet'
+import ScriptTag from 'react-script-tag';
+import headerJS ffrom '../js/header.js'
 
 const Menu = () => {
   return (
-     <div>
-    <div class="topnav" id="myTopnav">
-        <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
+    <div>
+        <div class="topnav" id="myTopnav">
+            <a href="#home" class="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
     </div>
-    </div>
+    <ScriptTag isHydrating={true} type="text/javascript" src={headerJS} />
   )
 }
 
