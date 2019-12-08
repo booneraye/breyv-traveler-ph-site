1b33d3792c1db1130de5c1ec7ab9551c2bc4ee3d
booneraye <booneraye@gmail.com>
2019-12-09 03:58:11 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,77 +1,26 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
-
-const Header = styled.header`
-  background: ${props => props.theme.colors.base};
-  width: 100%;
-  padding: 1.5em 0;
-`
-const Nav = styled.nav`
-  width: 100%;
-  max-width: ${props => props.theme.sizes.maxWidth};
-  margin: 0 auto;
-  padding: 0 1.5em;
-
-  ul {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  li {
-    display: inline-block;
-    margin-left: 1em;
-    &:first-child {
-      position: relative;
-      margin: 0;
-      flex-basis: 100%;
-    }
-  }
-
-  a {
-    text-decoration: none;
-    color: DarkGray;
-    font-weight: 600;
-    transition: all 0.2s;
-    border-bottom: 2px solid ${props => props.theme.colors.base};
-    &:hover {
-      color: white;
-    }
-  }
-`
-
-const activeLinkStyle = {
-  color: 'white',
-}
+import header from '../styles/header.css'
 
 const Menu = () => {
   return (
-    <Header>
-      <Nav>
-        <ul>
-          <li>
-            <Link to="/" activeStyle={activeLinkStyle}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <a href='https://breyvtraveler.com/biyaheroes' target='_blank'>
-              Tickets!
-            </a>
-          </li>
-          <li>
-            <Link to="/about/" activeStyle={activeLinkStyle}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact/" activeStyle={activeLinkStyle}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </Nav>
-    </Header>
+      <header>
+    <div class="container">
+      <div class="logo-box">
+        <a href="/">
+          <img src="../static/logos/logo1.png">
+        </a>
+      </div>
+      <nav>
+      <ul>
+        <li><a href="">home</a></li>
+        <li><a href="">work</a></li>
+        <li><a href="">product</a></li>
+        <li><a href="">about</a></li>
+        <li><a href="">contact</a></li>
+     </ul>
+    </nav>
+    </div>
+  </header>
   )
 }
 
