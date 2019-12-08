2d75ad11917878f027bfe9801a84d71c05ffd771
booneraye <booneraye@gmail.com>
2019-12-09 05:19:32 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,7 +16,7 @@ const Menu = () => {
             <a href="https://breyvtraveler.com/" class="active">Home</a>
             <a href="https://breyvtraveler.com/contact">Contact</a>
             <a href="https://breyvtraveler.com/about">About</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <a href="javascript:void(0);" class="icon" onclick={myFunction()}>
                 <i class="fa fa-bars"></i>
             </a>
         </div>
