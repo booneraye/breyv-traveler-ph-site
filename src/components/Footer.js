b736b2c7c883e392af2697eca3009e092ee2e7f0
booneraye <booneraye@gmail.com>
2019-12-09 03:09:23 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,46 +2,56 @@ import React from 'react'
 import footer from '../styles/footer.css'
 
 const Footer = () => (
-    <React.Fragment>
-        <footer class="site-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <h6>About</h6>
-                        <p class="text-justify">Brave traveler ph started as a dream that travelling, especially in the Philippines will soon become easier and adventurous by giving the people tips, encouragement, ideas and support on how to explore the beauty of our own country. It is an idealism that you don't have need a lot of funds or money to be able to travel, you just need to be BRAVE enough to do so.</p>
-                    </div>
+    <footer class="footer-distributed">
+        <div class="footer-left">
+            <h3>Brave Traveler PH</h3>
+            <p class="footer-links">
+		        <a href="#">Home</a>
+	            ·
+		        <a href="#">Blog</a>
+	            ·
+		        <a href="#">Pricing</a>
+	            ·
+		        <a href="#">About</a>
+	            ·
+		        <a href="#">Faq</a>
+	            ·
+		        <a href="#">Contact</a>
+		    </p>
 
-                    <div class="col-xs-6 col-md-3">
-                        <h6>Quick Links</h6>
-                        <ul class="footer-links">
-                            <li><a href="https://breyvtraveler.com/about/">About Us</a></li>
-                            <li><a href="https://breyvtraveler.com/contact/">Contact Us</a></li>
-                            <li><a href="https://breyvtraveler.com/privacy-policy/">Privacy Policy</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <hr>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-6 col-xs-12">
-                        <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved by
-                        <a href="#">BRAvE TRAVELER PH</a>.
-                        </p>
-                    </div>
+		    <p class="footer-company-name">brave traveler ph &copy; 2019</p>
+		</div>
 
-                    <div class="col-md-4 col-sm-6 col-xs-12">
-                        <ul class="social-icons">
-                            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </ React.Fragment>
+		<div class="footer-center">
+		    <div>
+		        <i class="fa fa-map-marker"></i>
+		        <p><span>Balete Street</span> Caloocan City, NCR</p>
+		    </div>
+		    <div>
+		        <i class="fa fa-phone"></i>
+		        <p>+63 9099353050</p>
+		    </div>
+            <div>
+		        <i class="fa fa-envelope"></i>
+		        <p><a href="mailto:support@company.com">contact@webdevtrick.com</a></p>
+		    </div>
+		</div>
+
+		<div class="footer-right">
+		    <p class="footer-company-about">
+		        <span>About</span>
+	            Brave traveler ph started as a dream that travelling, especially in the Philippines will soon become easier and adventurous by giving the people tips, encouragement, ideas and support on how to explore the beauty of our own country. It is an idealism that you don't have need a lot of funds or money to be able to travel, you just need to be BRAVE enough to do so.
+		    </p>
+
+		    <div class="footer-icons">
+ 		        <a href="#"><i class="fa fa-facebook"></i></a>
+		        <a href="#"><i class="fa fa-twitter"></i></a>
+		        <a href="#"><i class="fa fa-linkedin"></i></a>
+		        <a href="#"><i class="fa fa-github"></i></a>
+		    </div>
+		</div>
+
+	</footer>
 )
 
 export default Footer
