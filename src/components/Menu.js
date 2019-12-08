1acc2a62062c32da9e7b98470db70df0427e0b4b
booneraye <booneraye@gmail.com>
2019-12-09 05:17:26 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import header from '../styles/header.css'
 
 const Menu = () => {
-    function myFunction{
+    function myFunction(){
         var x = document.getElementById("myTopnav");
         if (x.className === "topnav") {
           x.className += " responsive";
