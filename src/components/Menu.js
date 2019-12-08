6e91b8b9731ecc2502b65f8d0572b5f104254be6
booneraye <booneraye@gmail.com>
2019-12-09 04:24:18 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import omegaCSS from '../styles/assets/css/omega.css'
-import omegaJS from '../styles/assets/js/omega.js'
+import header from '../styles/header.css'
 
 const Menu = () => {
   return (
-      <div id="omega">
-  <div id="omega-content">
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-    <div class="omega-links">
-      <a href="#" class="button">Link 1</a>
-      <a href="#" class="button button-outline ">Link 2</a>
+      <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
     </div>
-
-  </div>
-  <button id="omega-button">&#9776;</button>
-  <div id="omega-sidebar">
-    <div id="omega-sidebar-header"></div>
-    <div id="omega-sidebar-body"></div>
-  </div>
-  <div id="omega-overlay"></div>
-</div>
   )
 }
 
