39bc026f18e11ec1173c94c334735e6785bf6077
booneraye <booneraye@gmail.com>
2019-12-09 04:29:29 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import header from '../styles/header.css'
+import Helmet from 'react-helmet'
 
 const Menu = () => {
   return (
-      <div class="topnav" id="myTopnav">
+     <div>
+    <div class="topnav" id="myTopnav">
         <a href="#home" class="active">Home</a>
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
@@ -11,6 +13,23 @@ const Menu = () => {
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
+    </div>
+    <Helmet>
+        <script>
+            {
+                `
+                function myFunction() {
+                    var x = document.getElementById("myTopnav");
+                    if (x.className === "topnav") {
+                        x.className += " responsive";
+                    } else {
+                        x.className = "topnav";
+                    }
+                }
+                `
+            }
+        </script>
+    </Helmet>
     </div>
   )
 }
