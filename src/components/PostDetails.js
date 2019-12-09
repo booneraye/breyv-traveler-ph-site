e58a5dac575c349a45233c1d1a91e1f5aa8f6c75
booneraye <booneraye@gmail.com>
2019-12-09 16:45:36 +0800

    update share button

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -9,6 +9,14 @@ const Wrapper = styled.div`
   }
 `
 
+const ShareButton = styled.div`
+  margin: 0 auto 2em;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
+  span {
+    margin: 0 0.5rem;
+  }
+`
+
 const Date = styled.p`
   display: inline-block;
 `
@@ -23,13 +31,10 @@ const PostDetails = props => {
       <Date>📅 {props.date}</Date>
       <span>•</span>
       <ReadingTime>{`⏱️${props.timeToRead} min read `}</ReadingTime>
-    </Wrapper>
-    <Wrapper>
         <div
             class="fb-like"
             data-href={props.shareLink}
             data-layout="button"
-            data-width=""
             data-size="large"
             data-action="like"
             data-share="true">
