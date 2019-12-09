85de67e14cd55daa3fbdde41000cf38386c8dd2c
booneraye <booneraye@gmail.com>
2019-12-09 18:49:46 +0800

    update header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,11 +6,11 @@ const Menu = () => {
   return (
 
     <header class="header">
-        <a href="" class="logo"><img src='https://breyvtraveler.com/logos/brave_black.png' class='logos'/></a>
+        <a href="https://breyvtraveler.com/" class="logo"><img src='https://breyvtraveler.com/logos/brave_black.png' class='logos'/></a>
         <input class="menu-btn" type="checkbox" id="menu-btn" />
         <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
         <ul class="menu">
-            <li><a href="ttps://breyvtraveler.com/about/">About</a></li>
+            <li><a href="https://breyvtraveler.com/about/">About</a></li>
             <li><a href="https://breyvtraveler.com/biyaheroes" target='_blank'>Buy Tickets</a></li>
             <li><a href="https://breyvtraveler.com/contact/">Contact</a></li>
         </ul>
